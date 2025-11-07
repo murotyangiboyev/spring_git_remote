fd8f63108d1ec66b3e6327a4049cd0b5444bbcfd
Murot Yangiboyev <murotyangiboyev77@gmail.com>
2025-11-07 20:57:55 +0500

    second commit

--- src/main/java/com/example/spring_git_remote/SpringGitRemoteApplication.java
+++ src/main/java/com/example/spring_git_remote/SpringGitRemoteApplication.java
@@ -9,6 +9,8 @@ public class SpringGitRemoteApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringGitRemoteApplication.class, args);
         System.out.println("hello world");
+
+        System.out.println("hello world2");
     }
 
 }
