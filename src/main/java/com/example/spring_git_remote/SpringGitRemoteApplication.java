763db7edd6517eb523faba700c5cbfe33c0f76ba
Murot Yangiboyev <murotyangiboyev77@gmail.com>
2025-11-07 20:52:07 +0500

    first commit

---
+++ src/main/java/com/example/spring_git_remote/SpringGitRemoteApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_git_remote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGitRemoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGitRemoteApplication.class, args);
+    }
+
+}
