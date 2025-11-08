c3d3e2c456c7ec98eea4633da771ecf8a2487dd1
Murot Yangiboyev <murotyangiboyev77@gmail.com>
2025-11-08 10:15:52 +0500

    adding Users

---
+++ src/main/java/com/example/spring_git_remote/Users.java
@@ -0,0 +1,12 @@
+package com.example.spring_git_remote;
+
+public class Users {
+
+    private String name;
+    private String password;
+
+    public Users(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+}
